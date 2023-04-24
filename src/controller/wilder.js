ca628a15c9c5538d10ebc7f17530111b14413717
jorisgrls <jorisgrilleres@gmail.com>
2023-04-24 16:25:54 +0200

    Replace then/catch by async/await & try/catch

--- src/controller/wilder.js
+++ src/controller/wilder.js
@@ -2,50 +2,38 @@ const dataSource = require("../utils").dataSource;
 const Wilder = require("../entity/Wilder");
 
 module.exports = {
-    create: (req, res) => {
-        dataSource
-            .getRepository(Wilder)
-            .save(req.body)
-            .then(()=> {
-                res.send("Created wilder");
-            })
-            .catch(()=>{
-                res.send("Error while creating wilder");
-            })
+    create: async (req, res) => {
+        try {
+            await dataSource.getRepository(Wilder).save(req.body);
+            res.send("Created wilder");
+        } catch (error) {
+            res.send("Error while creating wilder");
+        }
     },
-    findAll: (req, res) => {
-        dataSource
-            .getRepository(Wilder)
-            .find()
-            .then((wilders)=> {
-                res.send(wilders);
-            })
-            .catch(()=>{
-                res.send("Error while fetching wilders");
-            })
+    findAll: async (req, res) => {
+        try {
+            const wilders = await dataSource.getRepository(Wilder).find();
+            res.send(wilders);
+        } catch (error) {
+            res.send("Error while fetching wilders");
+        }
     },
-    delete: (req, res) => {
-        dataSource
-            .getRepository(Wilder)
-            .delete(req.params.id)
-            .then(()=> {
-                res.send("Deleted wilder");
-            })
-            .catch(()=>{
-                res.send("Error while deleting wilder");
-            })
+    delete: async (req, res) => {
+        try {
+            await dataSource.getRepository(Wilder).delete(req.params.id);
+            res.send("Deleted wilder");
+        } catch (error) {
+            res.send("Error while deleting wilder");
+        }
     },
-    update: (req, res) => {
+    update: async (req, res) => {
         const id = req.params.id;
         const updatedFields = req.body;
-        dataSource
-            .getRepository(Wilder)
-            .update(id, updatedFields)
-            .then(()=> {
-                res.send("Updated wilder");
-            })
-            .catch(()=>{
-                res.send("Error while updating wilder");
-            })
+        try {
+            await dataSource.getRepository(Wilder).update(id, updatedFields);
+            res.send("Updated wilder");
+        } catch (error) {
+            res.send("Error while updating wilder");
+        }
     }
 };
